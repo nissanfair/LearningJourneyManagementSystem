77cd4367b4a96aed65f0f3a5d333e1db22a59830
w4ff13xp <junhuilee98@yahoo.com.sg>
2022-09-29 22:08:25 +0800

    Backend api_app updated courses and skills to be linked with courseskill

--- api_app.py
+++ api_app.py
@@ -45,17 +45,20 @@ class Skill(db.Model):
     skill_id = db.Column(db.Integer, primary_key=True)
     skill_name = db.Column(db.String(255), nullable=False)
     roleskills = db.relationship('RoleSkill', backref='skill', lazy=True)
+    courseskills = db.relationship('CourseSkill', backref='skill', lazy=True)
 
-    def __init__(self, skill_id, skill_name, roleskills = []):
+    def __init__(self, skill_id, skill_name, roleskills = [], courseskills = []):
         self.skill_id = skill_id
         self.skill_name = skill_name
         self.roleskills = roleskills
+        self.courseskills = courseskills
 
     def json(self):
         return {
                 "skill_id": self.skill_id,
                 "skill_name": self.skill_name,
-                "roleskills": [roleskill.json() for roleskill in self.roleskills]
+                "roleskills": [roleskill.json() for roleskill in self.roleskills],
+                "courseskills": [courseskill.json() for courseskill in self.courseskills]
             }
 
 class RoleSkill(db.Model):
@@ -124,6 +127,24 @@ class Staff(db.Model):
             "role": self.role
         }
 
+class CourseSkill(db.Model):
+    __tablename__ = 'courseskill'
+    csid = db.Column(db.Integer, primary_key=True)
+    skill_id = db.Column(db.Integer, db.ForeignKey('skill.skill_id'), nullable=False)
+    course_id = db.Column(db.String(20), db.ForeignKey('course.course_id'), nullable=False)
+
+    def __init__(self, csid, skill_id, course_id):
+        self.csid = csid
+        self.skill_id = skill_id
+        self.course_id = course_id
+
+    def json(self):
+        return {
+                "csid": self.csid,
+                "skill_id": self.skill_id,
+                "course_id": self.course_id
+            }
+
 class Course(db.Model):
     course_id = db.Column(db.String(20), primary_key=True)
     course_name = db.Column(db.String(50), nullable=False)
@@ -132,8 +153,9 @@ class Course(db.Model):
     course_type = db.Column(db.String(10))
     course_category = db.Column(db.String(50))
     registrations = db.relationship('Registration', backref='Course', lazy=True)
+    courseskills = db.relationship('CourseSkill', backref='course', lazy=True)
 
-    def __init__(self,course_id,course_name,course_desc,course_status,course_type,course_category,registrations = list()):
+    def __init__(self,course_id,course_name,course_desc,course_status,course_type,course_category,registrations = list(),courseskills = list()):
         self.course_id = course_id
         self.course_name = course_name
         self.course_desc = course_desc
@@ -141,6 +163,19 @@ class Course(db.Model):
         self.course_type = course_type
         self.course_category = course_category
         self.registrations = registrations
+        self.courseskills = courseskills
+
+    def json(self):
+        return {
+            "course_id": self.course_id,
+            "course_name": self.course_name,
+            "course_desc": self.course_desc,
+            "course_status": self.course_status,
+            "course_type": self.course_type,
+            "course_category": self.course_category,
+            "registrations": [registration.json() for registration in self.registrations],
+            "courseskills": [courseskill.json() for courseskill in self.courseskills]
+        }
 
 class Registration(db.Model):
     reg_id = db.Column(db.Integer, primary_key=True)
@@ -157,6 +192,15 @@ class Registration(db.Model):
         self.completion_status = completion_status
         self.course_id = course_id
         self.staff_id = staff_id
+
+    def json(self):
+        return {
+            "reg_id": self.reg_id,
+            "reg_status": self.reg_status,
+            "completion_status": self.completion_status,
+            "course_id": self.course_id,
+            "staff_id": self.staff_id
+        }
 
 # db.create_all()
 
@@ -196,11 +240,13 @@ def add_values(): #THIS IS EXAMPLE TO ADD VALUES TO DB (CURRENTLY NOT USED AS WE
 def home():
     return """
     <h1>Hello! this links are to test the backend, expect json files from them.</h1>
+    <a href='/course'>get courses</a>
     <a href='/staff'>get staffs</a>
     <a href='/role'>get roles</a>
     <a href='/jobrole'>get jobroles</a>
     <a href='/skill'>get skills</a>
     <a href='/roleskill'>get roleskills</a>
+    <a href='/courseskill'>get courseskills</a>
     """
 
 @app.route('/skill')
@@ -391,6 +437,64 @@ def getjobrolebyid(jobrole_id):
         }
     ), 404
 
+@app.route('/course')
+def course():
+    courses = Course.query.all()
+
+    if len(courses):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "courses": [course.json() for course in courses]
+                }
+            }
+        )
+
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no courses."
+        }
+    ), 404
+
+@app.route('/course/<string:course_id>')
+def find_course(course_id):
+    course = Course.query.filter_by(course_id=course_id).first()
+    if course:
+        return jsonify(
+            {
+                "code": 200,
+                "data": course.json()
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "Course not found."
+        }
+    ), 404
+
+@app.route('/courseskill')
+def courseskill():
+    courseskills = CourseSkill.query.all()
+
+    if len(courseskills):
+        return jsonify(
+            {
+                "code": 200,
+                "data": {
+                    "courseskills": [courseskill.json() for courseskill in courseskills]
+                }
+            }
+        )
+
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There are no courseskills."
+        }
+    ), 404
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)

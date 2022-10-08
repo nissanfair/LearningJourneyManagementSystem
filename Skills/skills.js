33d9951d1b1fb806af014b66a36f1989c1969111
Marcus Leo <marcleo97@hotmail.com>
2022-10-08 16:29:48 +0800

    Minor changes to prompts
    added code that only allows the user to dismiss the prompt msg by click on the button provded

--- Skills/skills.js
+++ Skills/skills.js
@@ -4,16 +4,15 @@ const app1 = Vue.createApp({
     return {
       // name:value pairs here
       // cannot use variables in another variable as you are declaring here
-      skillName : "",
-      skillDesc : "",
-      disabled : false,
-      message : "",
+      skillName: "",
+      skillDesc: "",
+      disabled: false,
+      message: "",
       skills: "", // Placeholder for now it is to hold all the skills coming from the back end
     };
   },
   methods: {
     del(id) {
-
       //Confirmation prompt for deletion
       Swal.fire({
         title: "Are you sure?",
@@ -24,9 +23,7 @@ const app1 = Vue.createApp({
         cancelButtonColor: "#d33",
         confirmButtonText: "Yes, delete it!",
       }).then((result) => {
-
         if (result.isConfirmed) {
-
           //use axios to pass the data over
           url = "http://localhost:5000/skill/" + id + "/softdelete";
           axios
@@ -35,21 +32,23 @@ const app1 = Vue.createApp({
               // process response.data object
               stat = response.data.code;
               if (stat) {
-                Swal.fire("Deleted!", "Skill has been deleted.", "success").then((result) => {
+                Swal.fire({
+                  title: "Deleted!",
+                  text: "Skill has been deleted.",
+                  icon: "success",
+                  allowOutsideClick: false,
+                }).then((result) => {
                   if (result.isConfirmed) {
-    
-                    //refresh the page 
+                    //refresh the page
                     location.reload();
                   }
-                })
-              } 
-              else {
+                });
+              } else {
                 Swal.fire({
                   icon: "error",
                   title: "Oops...",
                   text: "Something went wrong!",
                 });
-
               }
             })
             .catch((error) => {
@@ -63,74 +62,72 @@ const app1 = Vue.createApp({
         }
       });
     },
-    cancel(){
-      this.skillDesc =""
-      this.skillName =""
-
+    cancel() {
+      this.skillDesc = "";
+      this.skillName = "";
     },
-    create(){
-      this.disabled = true
+    create() {
+      this.disabled = true;
 
-      //Axios post 
-      url="http://localhost:5000/skill"
-      //check if fields are not empty 
-      if(this.skillName.length == 0 || this.skillDesc.length == 0 ){
+      //Axios post
+      url = "http://localhost:5000/skill";
+      //check if fields are not empty
+      if (this.skillName.length == 0 || this.skillDesc.length == 0) {
         Swal.fire({
           icon: "error",
           title: "Oops...",
           text: "All fields must be filled up!",
         });
 
-        this.disabled = false
-      }
-      else{
-        axios.post(url, {
-          skill_name : this.skillName,
-          skill_desc : this.skillDesc
+        this.disabled = false;
+      } else {
+        axios
+          .post(url, {
+            skill_name: this.skillName,
+            skill_desc: this.skillDesc,
           })
-          .then(response => {
-          // process response.data
-          console.log("create response:" + response.data.code)
-          stat = response.data.code;
-          if (stat) {
-            Swal.fire("Created!", "Skill has been created.", "success").then((result) => {
-              if (result.isConfirmed) {
-  
-                this.disabled = false
-                //refresh the page 
-                location.reload();
-              }
-            })
-          } 
-  
+          .then((response) => {
+            // process response.data
+            console.log("create response:" + response.data.code);
+            stat = response.data.code;
+            if (stat) {
+              Swal.fire({
+                title: "Created!",
+                text: "Skill has been created.",
+                icon: "success",
+                allowOutsideClick: false,
+                
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.disabled = false;
+                  //refresh the page
+                  location.reload();
+                }
+              });
+            }
           })
-          .catch(error => {
-          // process error object
-          this.disabled = false
-          console.log(error.response.status)
-          
-          //When skill already exists
-          if(error.response.status){
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Skill already Exists!",
-          });
-        }
-        else{
-          Swal.fire({
-            icon: "error",
-            title: "Oops...",
-            text: "Oops Something Went Wrong!",
-          });
-        }
-          
-          });
+          .catch((error) => {
+            // process error object
+            this.disabled = false;
+            console.log(error.response.status);
 
+            //When skill already exists
+            if (error.response.status) {
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Skill already Exists!",
+              });
+            } else {
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Oops Something Went Wrong!",
+              });
+            }
+          });
       }
-
-      
-    }
+    },
   },
   created() {
     url = "http://localhost:5000/skill";
@@ -138,22 +135,21 @@ const app1 = Vue.createApp({
       .get(url)
       .then((response) => {
         // process response.data object
-        console.log(response.data.code)
-        if(response.data.code == 200){
+        console.log(response.data.code);
+        if (response.data.code == 200) {
           this.skills = response.data.data.skills;
         }
-        //When all skills are softdeleted 
-        else{
-          this.message = "<p>There are no skills currently available</p>"
+        //When all skills are softdeleted
+        else {
+          this.message = "<p>There are no skills currently available</p>";
         }
-        
       })
       .catch((error) => {
         // process error object
-        console.log(error.response.status)
-        //When skills database is empty 
-        if(error.response.status == 404){
-          this.message ="<p> There is currently no skills created </p>"
+        console.log(error.response.status);
+        //When skills database is empty
+        if (error.response.status == 404) {
+          this.message = "<p> There is currently no skills created </p>";
         }
       });
   },
